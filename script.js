5efd05b617f1f77fb1bb89f6e3845a67cf976d70
Mindy D Tran <mindytran98@gmail.com>
2023-12-10 18:12:02 -0500

    change answer bank info

--- script.js
+++ script.js
@@ -1,56 +1,34 @@
 //styling page
-//store answer area section
-
-createAnswerArea();
-
-function createAnswerArea() {
-  const answerArea = document.getElementById("answer-area");
-
-  const div = document.createElement("div");
-  div.classList.add("img-container");
-
-  const frag = document.createDocumentFragment();
-
-  const img = document.createElement("img");
-  changeImgSrc(img, "./img/kaveh.png");
-  div.appendChild(img);
-
-  const p = document.createElement("p");
-  div.appendChild(p);
-
-  frag.appendChild(div);
-
-  answerArea.appendChild(frag);
-}
+// temp to see the layout of page
+const img = document.querySelector(".img-area img");
+changeImgSrc(img, "./img/kaveh.png");
 
 function changeImgSrc(img, imgSrc) {
   img.setAttribute("src", imgSrc);
 }
 //game section
+
 const answerBank = [
-  // index: 0
   {
-    name: "pikachu",
-    imgSrc:
-      "https://cdn.vox-cdn.com/thumbor/PqAwdNpc7p-5GUCSMxQPehSU-ck=/0x0:1920x1200/1200x800/filters:focal(810x375:1116x681)/cdn.vox-cdn.com/uploads/chorus_image/image/72524797/pikachu_artwork.0.jpg",
-  },
-  // index: 1
-  {
-    name: "togepi",
-    imgSrc:
-      "https://facts.net/wp-content/uploads/2023/07/16-facts-about-togepi-1689384445.jpg",
-  },
-  // index: 2
-  {
-    name: "electrode",
-    imgSrc:
-      "https://www.pcinvasion.com/wp-content/uploads/2023/01/pokemon-scarlet-violet-evolve-voltorb-into-electrode-result.jpg?fit=1200%2C675",
+    name: "diluc",
+    imgSrc: "./img/diluc.png",
   },
   {
-    name: "eevee",
-    imgSrc:
-      "https://assets.nintendo.com/image/upload/ar_16:9,b_auto:border,c_lpad/b_white/f_auto/q_auto/dpr_2.0/c_scale,w_400/ncom/software/switch/70010000000450/e8e1f203fc2d8ead2c7542e78e97675a01c0291180a7b8e1afe0d1488b05ada9",
+    name: "kaveh",
+    imgSrc: "./img/kaveh.png",
   },
+  {
+    name: "zhongli",
+    imgSrc: "./img/zhongli.png",
+  },
+  {
+    name: "neuvillette",
+    imgSrc: "./img/neuvillette.png",
+  },
+  {
+    name: "raiden shougun",
+    imgSrc: "./img/raiden.png"
+  }
 ];
 // store index of random answer
 const index = getRandomIndex(answerBank);
