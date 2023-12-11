d4ffb3d9185a400f9f9d2d47e996f8d58eceff89
Mindy D Tran <mindytran98@gmail.com>
2023-12-10 19:19:38 -0500

    create checkAnswer funciton

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
 // class to make answer object
 class Answer {
-    #name; #imgSrc;
-    constructor(name, imgSrc){
-        this.#name = name;
-        this.#imgSrc = imgSrc;
-    }
+  #name;
+  #imgSrc;
+  constructor(name, imgSrc) {
+    this.#name = name;
+    this.#imgSrc = imgSrc;
+  }
 
-    get name(){
-        return this.#name;
-    }
-    get imgSrc(){
-        return this.#imgSrc;
-    }
+  get name() {
+    return this.#name;
+  }
+  get imgSrc() {
+    return this.#imgSrc;
+  }
 }
 
 //styling page
@@ -22,39 +23,60 @@ changeImgSrc(img, "./img/kaveh.png");
 function changeImgSrc(img, imgSrc) {
   img.setAttribute("src", imgSrc);
 }
-//game section
 
-const answerBank = [ new Answer("diluc", "./img/diluc.png"), new Answer("alhaitham", "./img/alhaitham.png"), new Answer(["ayaka", "kamisato"], "./img/ayaka.png"),new Answer(["child", "tartaglia","ajax"],"./img/childe.png"), new Answer(["hutao", "hu", "tao"], "./img/hutao.png"), new Answer("kaveh", "./img/kaveh.png"), new Answer(["kokomi", "sangonomiya"], "./img/kokomi.png"), new Answer("neuvillette", "./img/neuvillette.png"), new Answer(["raiden", "shougun", "shogun", "shoogun", "ei"], "./img/raiden.png"), new Answer("xiao", "./img/xiao.png"), new Answer(["yae", "miko", "yaemiko"], "./img/yaemiko.png"), new Answer(["yoimiya", "naganohara"], "./img/yoimiya.png"), new Answer("zhongli", "./img/zhongli.png")
+//game section
+// some of the names are arrays because the character have multiple names
+const answerBank = [
+  new Answer("diluc", "./img/diluc.png"),
+  new Answer("alhaitham", "./img/alhaitham.png"),
+  new Answer(["ayaka", "kamisato"], "./img/ayaka.png"),
+  new Answer(["child", "tartaglia", "ajax"], "./img/childe.png"),
+  new Answer(["hutao", "hu", "tao"], "./img/hutao.png"),
+  new Answer("kaveh", "./img/kaveh.png"),
+  new Answer(["kokomi", "sangonomiya"], "./img/kokomi.png"),
+  new Answer("neuvillette", "./img/neuvillette.png"),
+  new Answer(
+    ["raiden", "shougun", "shogun", "shoogun", "ei"],
+    "./img/raiden.png"
+  ),
+  new Answer("xiao", "./img/xiao.png"),
+  new Answer(["yae", "miko", "yaemiko"], "./img/yaemiko.png"),
+  new Answer(["yoimiya", "naganohara"], "./img/yoimiya.png"),
+  new Answer("zhongli", "./img/zhongli.png"),
 ];
 
-// store index of random answer
+// store randomly generated question bank
 const questionsBank = createQuestionBank(answerBank);
-// store answer
-// const answer = answerBank[index].name;
+// store current question;
+let currentQuestion = 0;
+let userScore = 0;
 
 // create random questions
-function createQuestionBank (answerBank) {
-    const questionBankIndexes = [];
-    const questionsBank = [];
-    // add random indexes into questionBankIndexes
-    while(questionBankIndexes.length <5) {
-        const index = getRandomIndex(answerBank);
-        // checks if arr already contains index (don't want duplicates)
-        if(!(questionBankIndexes.includes(index))){
-            questionBankIndexes.push(index);
-        }
+function createQuestionBank(answerBank) {
+  const questionBankIndexes = [];
+  const questionsBank = [];
+  // add random indexes into questionBankIndexes
+  while (questionBankIndexes.length < 5) {
+    const index = getRandomIndex(answerBank);
+    // checks if arr already contains index (don't want duplicates)
+    if (!questionBankIndexes.includes(index)) {
+      questionBankIndexes.push(index);
     }
-    // add information into questionsBank
-    questionBankIndexes.forEach(index => questionsBank.push(answerBank[index]));
-    return questionsBank;
+  }
+  // add information into questionsBank
+  questionBankIndexes.forEach((index) => questionsBank.push(answerBank[index]));
+  return questionsBank;
 }
-
 // check if the user is right or wrong
-function checkAnswer(userGuess, answer) {
-    return userGuess.toLowerCase() === answer; 
+function checkAnswer(userGuess, answer, score) {
+   if(userGuess.toLowerCase() === answer){
+    score+=100;
+   } else {
+    score-=50;
+   }
+   return score;
 }
-
-//gets random index 
+//gets random index
 function getRandomIndex(arr) {
   /*Use Math.random() to generate random number (returns a float, aka a number with decimal points)
    * Multiply Math.random() by array's length to pick a number from 0 to array's length-1 (still a float)
