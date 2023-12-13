9d728b4f17d47b004c9b9e3a7b9ec209c3a28238
Mindy D Tran <mindytran98@gmail.com>
2023-12-12 21:57:47 -0500

    add losing voice line

--- script.js
+++ script.js
@@ -2,11 +2,13 @@
 class Answer {
   #name;
   #imgSrc;
-  #audioSrc;
-  constructor(name, imgSrc, audioSrc) {
+  #winAudio;
+  #loseAudio;
+  constructor(name, imgSrc, audioWin, audioLose) {
     this.#name = name;
     this.#imgSrc = imgSrc;
-    this.#audioSrc = audioSrc;
+    this.#winAudio = audioWin;
+    this.#loseAudio = audioLose;
   }
   get name() {
     return this.#name;
@@ -14,30 +16,96 @@ class Answer {
   get imgSrc() {
     return this.#imgSrc;
   }
-  get audioSrc(){
-    return this.#audioSrc;
+  get winAudio() {
+    return this.#winAudio;
+  }
+  get loseAudio(){
+    return this.#loseAudio;
   }
 }
 
 //game section
 // some of the names are arrays because the character have multiple names
 const answerBank = [
-  new Answer(["diluc"], "./img/diluc.png", "https://static.wikia.nocookie.net/gensin-impact/images/6/63/VO_Diluc_Hello.ogg/"),
-  new Answer(["alhaitham"], "./img/alhaitham.png", "https://static.wikia.nocookie.net/gensin-impact/images/a/a3/VO_Alhaitham_Hello.ogg"),
-  new Answer(["ayaka", "kamisato"], "./img/ayaka.png", "https://static.wikia.nocookie.net/gensin-impact/images/b/b8/VO_Kamisato_Ayaka_Hello.ogg"),
-  new Answer(["childe", "tartaglia", "ajax"], "./img/childe.png", "https://static.wikia.nocookie.net/gensin-impact/images/0/0d/VO_Tartaglia_Hello.ogg/"),
-  new Answer(["hu", "tao", "hutao"], "./img/hutao.png", "https://static.wikia.nocookie.net/gensin-impact/images/2/28/VO_Hu_Tao_Hello.ogg"),
-  new Answer(["kaveh"], "./img/kaveh.png", "https://static.wikia.nocookie.net/gensin-impact/images/1/1f/VO_Kaveh_Hello.ogg/"),
-  new Answer(["kokomi", "sangonomiya"], "./img/kokomi.png", "https://static.wikia.nocookie.net/gensin-impact/images/8/8e/VO_Sangonomiya_Kokomi_Hello.ogg"),
-  new Answer(["neuvillette"], "./img/neuvillette.png", "https://static.wikia.nocookie.net/gensin-impact/images/5/53/VO_Neuvillette_Hello.ogg"),
+  new Answer(
+    ["alhaitham"],
+    "./img/alhaitham.png",
+    "https://static.wikia.nocookie.net/gensin-impact/images/a/a3/VO_Alhaitham_Hello.ogg"
+    ,"https://static.wikia.nocookie.net/gensin-impact/images/e/e6/VO_Alhaitham_Ally_at_Low_HP_02.ogg/"
+  ),
+  new Answer(
+    ["ayaka", "kamisato"],
+    "./img/ayaka.png",
+    "https://static.wikia.nocookie.net/gensin-impact/images/b/b8/VO_Kamisato_Ayaka_Hello.ogg"
+    ,"https://static.wikia.nocookie.net/gensin-impact/images/a/a1/VO_Kamisato_Ayaka_Heavy_Hit_Taken_02.ogg/"
+  ),
+  new Answer(
+    ["childe", "tartaglia", "ajax"],
+    "./img/childe.png",
+    "https://static.wikia.nocookie.net/gensin-impact/images/0/0d/VO_Tartaglia_Hello.ogg/"
+    ,"https://static.wikia.nocookie.net/gensin-impact/images/9/91/VO_Tartaglia_Ally_at_Low_HP_01.ogg/"
+  ),
+  new Answer(
+    ["diluc"],
+    "./img/diluc.png",
+    "https://static.wikia.nocookie.net/gensin-impact/images/6/63/VO_Diluc_Hello.ogg/"
+    ,"https://static.wikia.nocookie.net/gensin-impact/images/3/3d/VO_Diluc_Elemental_Burst_02.ogg/"
+  ),
+  new Answer(
+    ["hu", "tao", "hutao"],
+    "./img/hutao.png",
+    "https://static.wikia.nocookie.net/gensin-impact/images/2/28/VO_Hu_Tao_Hello.ogg"
+    ,"https://static.wikia.nocookie.net/gensin-impact/images/7/75/VO_Hu_Tao_Low_HP_01.ogg/"
+  ),
+  new Answer(
+    ["kaveh"],
+    "./img/kaveh.png",
+    "https://static.wikia.nocookie.net/gensin-impact/images/1/1f/VO_Kaveh_Hello.ogg/"
+    ,"https://static.wikia.nocookie.net/gensin-impact/images/f/f3/VO_Kaveh_Low_HP_03.ogg/"
+  ),
+  new Answer(
+    ["kokomi", "sangonomiya"],
+    "./img/kokomi.png",
+    "https://static.wikia.nocookie.net/gensin-impact/images/8/8e/VO_Sangonomiya_Kokomi_Hello.ogg"
+    ,"https://static.wikia.nocookie.net/gensin-impact/images/2/29/VO_Sangonomiya_Kokomi_Low_HP_02.ogg/"
+  ),
+  new Answer(
+    ["neuvillette"],
+    "./img/neuvillette.png",
+    "https://static.wikia.nocookie.net/gensin-impact/images/5/53/VO_Neuvillette_Hello.ogg"
+    ,"https://static.wikia.nocookie.net/gensin-impact/images/7/74/VO_Neuvillette_Elemental_Skill_1_02.ogg/"
+  ),
   new Answer(
     ["raiden", "shougun", "shogun", "shoogun", "ei"],
-    "./img/raiden.png", "https://static.wikia.nocookie.net/gensin-impact/images/9/9a/VO_Raiden_Shogun_Hello.ogg/"
+    "./img/raiden.png",
+    "https://static.wikia.nocookie.net/gensin-impact/images/9/9a/VO_Raiden_Shogun_Hello.ogg/"
+    ,"https://static.wikia.nocookie.net/gensin-impact/images/7/73/VO_Raiden_Shogun_Opening_Treasure_Chest_02.ogg/"
   ),
-  new Answer(["xiao"], "./img/xiao.png", "https://static.wikia.nocookie.net/gensin-impact/images/f/ff/VO_Xiao_Hello.ogg/"),
-  new Answer(["yae", "miko", "yaemiko"], "./img/yaemiko.png", "https://static.wikia.nocookie.net/gensin-impact/images/0/08/VO_Yae_Miko_Hello.ogg/"),
-  new Answer(["yoimiya", "naganohara"], "./img/yoimiya.png", "https://static.wikia.nocookie.net/gensin-impact/images/b/b2/VO_Yoimiya_Hello.ogg/"),
-  new Answer(["zhongli"], "./img/zhongli.png", "https://static.wikia.nocookie.net/gensin-impact/images/7/7b/VO_Zhongli_Hello.ogg/"),
+  new Answer(
+    ["xiao"],
+    "./img/xiao.png",
+    "https://static.wikia.nocookie.net/gensin-impact/images/f/ff/VO_Xiao_Hello.ogg/"
+    ,"https://static.wikia.nocookie.net/gensin-impact/images/2/26/VO_Xiao_Ally_at_Low_HP_01.ogg/",
+    "https://static.wikia.nocookie.net/gensin-impact/images/2/26/VO_Xiao_Ally_at_Low_HP_01.ogg/"
+  ),
+  new Answer(
+    ["yae", "miko", "yaemiko"],
+    "./img/yaemiko.png",
+    "https://static.wikia.nocookie.net/gensin-impact/images/0/08/VO_Yae_Miko_Hello.ogg/"
+    ,"https://static.wikia.nocookie.net/gensin-impact/images/f/f4/VO_Yae_Miko_Light_Hit_Taken_02.ogg/"
+  ),
+  new Answer(
+    ["yoimiya", "naganohara"],
+    "./img/yoimiya.png",
+    "https://static.wikia.nocookie.net/gensin-impact/images/b/b2/VO_Yoimiya_Hello.ogg/"
+    ,"https://static.wikia.nocookie.net/gensin-impact/images/4/42/VO_Yoimiya_Heavy_Hit_Taken_02.ogg/"
+  ),
+  new Answer(
+    ["zhongli"],
+    "./img/zhongli.png",
+    "https://static.wikia.nocookie.net/gensin-impact/images/7/7b/VO_Zhongli_Hello.ogg/"
+    ,"https://static.wikia.nocookie.net/gensin-impact/images/d/d9/VO_Zhongli_Heavy_Hit_Taken_01.ogg/"
+  ),
 ];
 
 // document elements
@@ -126,10 +194,10 @@ function showResults(result) {
   const h2 = document.createElement("h2");
   if (result) {
     h2.textContent = "You won!! Play again?";
+
   } else {
     h2.textContent = "You lost!! Play again?";
   }
-  playAudio();
   return h2;
 }
 
@@ -146,11 +214,16 @@ function displayReplayBtn(element) {
   element.appendChild(btn);
 }
 
-function playAudio(audioSrc) {
+function playAudio(result) {
+  let mySound;
   // have to use sound constructor to play sound (the easy way)
   // got it from this https://noaheakin.medium.com/adding-sound-to-your-js-web-app-f6a0ca728984
-  let mySound = new Audio(audioSrc);
-  return mySound;
+  if(result){
+    mySound = new Audio(question.winAudio);
+  }else{
+    mySound = new Audio(question.loseAudio);
+  }
+  return mySound.play();
 }
 form.addEventListener("submit", function (e) {
   e.preventDefault();
@@ -160,6 +233,6 @@ form.addEventListener("submit", function (e) {
     form.prepend(showResults(isUserRight));
     isClick = true;
     displayReplayBtn(form);
-    playAudio(question.audioSrc).play();
+    playAudio(isUserRight).catch;
   }
 });
